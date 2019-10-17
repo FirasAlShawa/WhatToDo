15f7cff39652e92870127ea9f31cb7857c4c0cd2
firasShawa <firas.shawa22@gmail.com>
2019-10-17 13:03:32 +0300

    Project Setup

---
+++ app/src/main/java/com/firasshawa/whattodo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.firasshawa.whattodo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
