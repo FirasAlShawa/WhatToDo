09a6d842332360231f19288510caccf6702bedde
firasShawa <firas.shawa22@gmail.com>
2019-10-17 14:21:40 +0300

    Sign up Layout

---
+++ app/src/main/java/com/firasshawa/whattodo/Activities/SignUp_Activity.java
@@ -0,0 +1,16 @@
+package com.firasshawa.whattodo.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.firasshawa.whattodo.R;
+
+public class SignUp_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_layout);
+    }
+}
