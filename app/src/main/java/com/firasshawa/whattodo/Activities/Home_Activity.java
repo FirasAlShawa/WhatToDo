bb8dcd11b2c8644d196ac8ee1ae9adff105db3ec
firasShawa <firas.shawa22@gmail.com>
2019-10-17 18:55:41 +0300

    Setup to BackDrop

---
+++ app/src/main/java/com/firasshawa/whattodo/Activities/Home_Activity.java
@@ -0,0 +1,17 @@
+package com.firasshawa.whattodo.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.firasshawa.whattodo.R;
+
+public class Home_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_layout);
+
+    }
+}
